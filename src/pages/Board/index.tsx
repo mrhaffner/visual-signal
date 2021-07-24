447c2232051bc51cc36227c19eb6a64b689b7aff
mrhaffner <mrhaffner13@gmail.com>
2021-07-24 16:56:26 -0500

    Re-ordering tasks wthin the same column now works again from the onDragEnd function within pages/Board/index.

--- src/pages/Board/index.tsx
+++ src/pages/Board/index.tsx
@@ -3,6 +3,7 @@ import { DragDropContext, DropResult, Droppable } from 'react-beautiful-dnd';
 import styled from 'styled-components';
 import ColumnList from './ColumnList';
 import boardData from '../../board-data';
+import { ColumnInterface } from '../../board-data';
 
 const Wrapper = styled.div`
   display: flex;
@@ -23,19 +24,42 @@ const Board = () => {
       return;
     }
 
-    // //reorder columns
+    //reorder columns
     if (type === 'column') {
       const newState = Array.from(state);
-      const splicedColumn = newState.splice(source.index, 1);
-      newState.splice(destination.index, 0, ...splicedColumn);
+      const splicedColumn = newState.splice(source.index, 1)[0]; //changed this form desctructure
+      newState.splice(destination.index, 0, splicedColumn);
       setState(newState);
       return;
     }
-    // /////////////////////////////
-    // //make these generic or use a typeguard?????
+    //this probably needs to change
+    const start = source.droppableId;
+    const finish = destination.droppableId;
+
+    if (start === finish) {
+      const column = state.filter((col) => col.id === start)[0];
+
+      const newTaskArray = Array.from(column.tasks);
+
+      const splicedTask = newTaskArray.splice(source.index, 1);
+      newTaskArray.splice(destination.index, 0, ...splicedTask);
+
+      const newColumn = {
+        ...column,
+        tasks: newTaskArray,
+      };
+
+      const newState = state.map((col) => (col.id === start ? newColumn : col));
+
+      setState(newState);
+      return;
+    }
+    /////////////////////////////
+    //make these generic or use a typeguard?????
     // const start = state.columns[source.droppableId as keyof ColumnsInterface];
     // const finish =
     //   state.columns[destination.droppableId as keyof ColumnsInterface];
+
     // //if moving within the same column
     // if (start === finish) {
     //   const column =
@@ -57,7 +81,7 @@ const Board = () => {
     //   setState(newState);
     //   return;
     // }
-    // //if moving to a new column
+    //if moving to a new column
     // const startTaskIds = Array.from(start.taskIds);
     // startTaskIds.splice(source.index, 1);
     // const newStart = {
