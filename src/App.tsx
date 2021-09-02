a16c0c1917cc7971c2b8a19e7f7d048355dd1d00
mrhaffner <mrhaffner13@gmail.com>
2021-09-02 18:00:59 -0500

    refactor: all pages main component now is named the same as the folder it contains.  This is imported and exported by the index file

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,9 @@ import { Switch, Route } from 'react-router-dom';
 import BoardProvider from './providers/BoardProvider';
 import Board from './pages/Board';
 import Boards from './pages/Boards';
-import NavBar from './components/NavBar';
 import LoadingProvider from './providers/LoadingProvider';
 import SignUp from './pages/SignUp';
-import LogIn from './pages/LogIn';
+import LogIn from './pages/LogIn/LogIn';
 
 const App = () => (
   <LoadingProvider>
