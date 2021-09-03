f4e8952a9e40fdc43275f8fe3266b14dab352d28
mrhaffner <mrhaffner13@gmail.com>
2021-09-03 18:59:38 -0500

    fix: removed broken import statement from SignInOrUpPage

--- src/components/SignInOrUpPage.tsx
+++ src/components/SignInOrUpPage.tsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import SignInOrUpFooter from './SignInOrUpFooter';
-import AtlassianBackground from './AtlassianBackground';
 
 const Wrapper = styled.div`
   background-color: #f9fafc;
