a45af13c1e551115ed34d3ef9a78540b6e7cea77
mrhaffner <mrhaffner13@gmail.com>
2021-10-02 13:37:47 -0500

    refactor: extracted a useNewBoardName hook from BoardProvider

---
+++ src/hooks/mutations/board/useNewBoardName.ts
@@ -0,0 +1,37 @@
+import { useMutation } from '@apollo/client';
+import { UPDATE_BOARD_NAME } from '../../../graphql/mutations/all';
+import { BoardInterface } from '../../../types';
+import useMemberContext from '../../useMemberContext';
+
+const useNewBoardName = (
+  board: BoardInterface | null,
+  setBoard: (input: BoardInterface) => void,
+) => {
+  const { setMemberFound } = useMemberContext();
+
+  const [updateBoardNameMutation] = useMutation(UPDATE_BOARD_NAME, {
+    onError: () => {
+      setMemberFound(false);
+    },
+  });
+
+  const newBoardName = (input: string) => {
+    if (!board) return;
+    try {
+      const updateObject = {
+        _id: board._id,
+        name: input,
+      };
+      updateBoardNameMutation({
+        variables: { updateBoardInput: updateObject },
+      });
+      setBoard({ ...board, name: input });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return newBoardName;
+};
+
+export default useNewBoardName;
