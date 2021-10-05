6ef65d7875b6cb440966e200f9135fc75fa248fe
mrhaffner <mrhaffner13@gmail.com>
2021-10-05 17:19:41 -0500

    refact: removed unused testing files. Moved colors list to new constants folder

--- src/Globalstyles.ts
+++ src/Globalstyles.ts
@@ -2,19 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 import { ColorKeys } from './types';
 import './fonts/fontFix.css';
-
-const colors = {
-  blue: 'rgb(0, 121, 191)',
-  orange: 'rgb(210, 144, 52)',
-  green: 'rgb(81, 152, 57)',
-  red: 'rgb(176, 70, 50)',
-  purple: 'rgb(137, 96, 158)',
-  pink: 'rgb(205, 90, 145)',
-  lime: 'rgb(75, 191, 107)',
-  sky: 'rgb(0, 174, 204)',
-  grey: 'rgb(131, 140, 145)',
-  default: '',
-};
+import { colors } from './constants/colors';
 
 interface GlobalProps {
   color: ColorKeys;
@@ -32,5 +20,5 @@ const GlobalStyles = createGlobalStyle<GlobalProps>`
         background-color: ${(props) => colors[props.color]};
     }
 `;
-//@ts-ignore
+
 export default GlobalStyles;
---
+++ src/constants/colors.ts
@@ -0,0 +1,12 @@
+export const colors = {
+  blue: 'rgb(0, 121, 191)',
+  orange: 'rgb(210, 144, 52)',
+  green: 'rgb(81, 152, 57)',
+  red: 'rgb(176, 70, 50)',
+  purple: 'rgb(137, 96, 158)',
+  pink: 'rgb(205, 90, 145)',
+  lime: 'rgb(75, 191, 107)',
+  sky: 'rgb(0, 174, 204)',
+  grey: 'rgb(131, 140, 145)',
+  default: '',
+};
