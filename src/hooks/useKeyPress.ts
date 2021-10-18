1abc4b24c2d6720a6ae8b97e418a99fdf0d9167d
mrhaffner <mrhaffner13@gmail.com>
2021-10-17 19:56:56 -0500

    fix: updated typing for hooks

--- src/hooks/useKeyPress.ts
+++ src/hooks/useKeyPress.ts
@@ -1,32 +1,29 @@
 import { useState, useEffect } from 'react';
 
-//update to typescript version
+interface Key {
+  key: string;
+}
 
-const useKeyPress = (targetKey: any) => {
-  // State for keeping track of whether key is pressed
-  const [keyPressed, setKeyPressed] = useState<boolean>(false);
-  // If pressed key is our target key then set to true
-  function downHandler({ key }: any) {
+const useKeyPress = (targetKey: string): boolean => {
+  const [keyPressed, setKeyPressed] = useState(false);
+  function downHandler({ key }: Key): void {
     if (key === targetKey) {
       setKeyPressed(true);
     }
   }
-  // If released key is our target key then set to false
-  const upHandler = ({ key }: any) => {
+  const upHandler = ({ key }: Key): void => {
     if (key === targetKey) {
       setKeyPressed(false);
     }
   };
-  // Add event listeners
   useEffect(() => {
     window.addEventListener('keydown', downHandler);
     window.addEventListener('keyup', upHandler);
-    // Remove event listeners on cleanup
     return () => {
       window.removeEventListener('keydown', downHandler);
       window.removeEventListener('keyup', upHandler);
     };
-  }, []); // Empty array ensures that effect is only run on mount and unmount
+  }, []);
   return keyPressed;
 };
 
